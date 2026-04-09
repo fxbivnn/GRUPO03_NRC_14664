b389c29741a64a86c7ca2d0cebf326d118f2fc97
Cristian <czick21y@gmail.com>
2026-04-09 17:00:33 -0500

    Subiendo aporte, Creando el ArregloProducto

--- src/gui/V1.java
+++ src/gui/V1.java
@@ -220,6 +220,7 @@ public class V1 extends JFrame implements ActionListener {
 		{
 			JOptionPane.showMessageDialog(this, "EL producto " + nom + " no se encuentra en la lista o no existe");
 		}
+		.
 	}
 	}
 }
